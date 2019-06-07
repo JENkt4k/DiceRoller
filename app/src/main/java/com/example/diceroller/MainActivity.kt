c9e9a8312cc99717cbe8851984bfda6e96ce4057
James Nelson <james.e.nelson@gmail.com>
2019-06-07 15:42:44 -0500

    Tutorial post step 20

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
